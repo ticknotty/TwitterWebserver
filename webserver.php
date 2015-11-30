bf42a5187a39d991f0db80d63f4907a127de33f6
ticknotty <nicktotty8@gmail.com>
2015-11-30 17:47:37 +0000

    Initial commit

---
+++ webserver.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+<meta charset="UTF-8">
+<title>Consuming Twitter Webserver</title>
+    <script>
+        window.onload = function() {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (xhttp.readyState == 4 && xhttp.status == 200) {
+                    document.getElementById("twitter").innerHTML = xhttp.responseText;
+                }
+            };
+            xhttp.open("GET", "http://nodeapp1202663.azurewebsites.net", true);
+            xhttp.send();
+        }
+
+    </script>
+
+
+</head>
+<body>
+<header>
+<h1>Header Div</h1>
+</header>
+<main>
+<h2> Main content </h2>
+</main>
+<aside id="twitter" >
+
+</aside>
+</body>
+</html>
